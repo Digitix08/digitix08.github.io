594a55f7a8d26efcc8a82adbfe34d30436f500ae
Gabi Dima <dg.ajtx@gmail.com>
2026-01-20 22:03:27 +0200

    added webbrowserCS pages added more useful links updated a template, hence all the changes added 1 more game (not made by me) unlinked from noip ddns (for now)

---
+++ index-casete/index.php
@@ -0,0 +1,247 @@
+<?php
+
+// Allow from any origin
+
+if(isset($_SERVER["HTTP_ORIGIN"]))
+
+{
+
+  if($_SERVER["HTTP_ORIGIN"] == 'http://localhost:8000' || str_contains($_SERVER["HTTP_ORIGIN"], "digitix08"))
+
+    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+
+}
+
+else
+
+{
+
+    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
+
+    header("Access-Control-Allow-Origin: *");
+
+}
+
+
+
+header("Access-Control-Allow-Credentials: true");
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
+
+{
+
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
+
+        header("Access-Control-Allow-Methods: GET, OPTIONS"); //Make sure you remove those you do not want to support
+
+
+
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
+
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+
+
+    //Just exit with 200 OK with the above headers for OPTIONS method
+
+    exit(0);
+
+}
+
+//From here, handle the request as it is ok
+
+
+
+function reader($tapes){
+
+  if (file_exists("./" . $tapes . ".txt")){
+
+  $txt_file = file_get_contents("./" . $tapes . ".txt");
+
+  $rows     = explode("\n", $txt_file);
+
+  array_shift($rows);
+
+  $last = 0;
+
+  $info = array();
+
+  $print = false;
+
+  $tape = 0;
+
+  $side = NULL;
+
+  $count = 0;
+
+  foreach($rows as $row => $data)
+
+  {
+
+    if(strlen($data)>0){
+
+      //get row data
+
+      $row_data = explode(' - ', $data);
+
+      if(strcasecmp($row_data[0], "alte shazamuri:")==0) break;
+
+      if(count($row_data)==1){
+
+        $name = explode(' ', $row_data[0]);
+
+        if(strcasecmp($name[0], "side")==0&&$tape>0){//side append
+
+          $sideName = $name[1];
+
+          $side++;
+
+          $info[$tape]["side_".$side] = array("0" => array("name" => $sideName));
+
+          $count = 1;
+
+          //echo 'side ' . $info[$row]['side'] . '<br />';
+
+        }
+
+        else if($row - $last >1){//tape append
+
+          $detail = $row_data[0];
+
+          if(strpos($detail, "---")!=0){
+
+            $print = true;
+
+            $detail = substr($detail, 0, strpos($detail, "---"));
+
+          }
+
+
+          if($print){
+            //some tapes have an * to show they were on the old site
+            if(substr($detail,0,1)=='*')$detail = substr($detail, 1, strlen($detail)-1);
+
+            //numeric id
+            //$idnr = null;
+            $lbrpos = strpos($detail, "]");
+            if($lbrpos!==false && $lbrpos<8){
+              $info[$row]["id"] = substr($detail, 1, $lbrpos - 1);
+              $detail = substr($detail, $lbrpos+1, strlen($detail) - $lbrpos);
+            }
+
+            $info[$row]["name"] = $detail;
+
+            $side = 0;
+
+            $tape = $row;
+
+          }
+
+          //echo 'id: ' . $info[$row]['id'] . '<br />';
+
+        }
+
+        else
+
+          if($print&&$side!=NULL){//melody append
+
+            $info[$tape]["side_".$side][$count]['nr'] = $count;
+
+            $info[$tape]["side_".$side][$count]['title'] = $row_data[0];
+
+            $count++;
+
+          }
+
+          //echo 'Title: ' . $info[$row]['title'] . '<br />';
+
+      }
+
+      else{
+
+        if($side!=NULL){//melody with author append
+
+          $info[$tape]["side_".$side][$count]['nr'] = $count;
+
+          $info[$tape]["side_".$side][$count]['author'] = $row_data[0];
+
+          $info[$tape]["side_".$side][$count]['title'] = $row_data[1];
+
+          $count++;
+
+        }
+
+        /*echo 'Row ' . $row . ' ID: ' . $info[$row]['author'] . '<br />';
+
+        echo 'Row ' . $row . ' NAME: ' . $info[$row]['title'] . '<br />';*/
+
+      }  
+
+      //echo '<br />';
+
+      $last = $row;;
+
+    }
+
+  }
+
+  return $info;
+
+  }
+
+  else sendError("failed to get list of tapes", 400, "invalid request sent");
+
+}
+
+// Always return JSON - no more IE8 HTML detection
+
+function sendResponse($data, $status = 200) {
+
+    http_response_code($status);
+
+
+
+    // Always JSON, always these headers for IE8 compatibility
+
+    header("Content-Type: application/json");
+
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+
+    header("Pragma: no-cache");
+
+    header("Expires: 0");
+
+
+
+    echo json_encode($data);
+
+}
+
+
+
+function sendError($message, $status = 400, $details = null) {
+
+    $error = ["error" => $message];
+
+    if ($details) $error["details"] = $details;
+
+    sendResponse($error, $status);
+
+}
+
+if(isset($_GET['list'])){
+
+  $request = $_GET['list'];
+  sendResponse(array("tapes" => array_values(reader($request))));
+
+}
+
+else sendError("failed to get list of tapes", 400, "invalid request sent");
+
+
+
+?>
